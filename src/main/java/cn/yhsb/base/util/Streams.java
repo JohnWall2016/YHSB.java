958e2f63456c09bbc3e04e74b0fe8adce3568751
John Wall <5672618@qq.com>
2020-03-22 22:35:54 +0800

    update

---
+++ src/main/java/cn/yhsb/base/util/Streams.java
@@ -0,0 +1,13 @@
+package cn.yhsb.base.util;
+
+import java.util.Iterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Streams {
+    public static <T> Stream<T> getStreamFromIterator(Iterator<T> it) {
+        var spliterator = Spliterators.spliteratorUnknownSize(it, 0);
+        return StreamSupport.stream(spliterator, false);
+    }
+}
